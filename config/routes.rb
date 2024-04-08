e50f60ecb9140831563a955f1d44bcf7331657fe
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-04-08 19:38:55 -0300

    chore: autalizacao projeto

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :posts do
+    resources :comments
+  end
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+ get "home", controller: "home",  action: "index", as:"home"
+
+ # get 'estudante', to: 'estudante#index', as: "estudantes"
+
+  root "home#index"
+end

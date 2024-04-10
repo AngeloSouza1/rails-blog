962a75b3fe9d26c68057002a2d344fcf95982ca6
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-04-10 19:37:20 -0300

    update3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Adiciona o hostname permitido
-  config.hosts << "rails-blog-10cc.onrender.com"
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
